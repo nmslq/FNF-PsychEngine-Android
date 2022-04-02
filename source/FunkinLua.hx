848a140b238086d85a6554cb89223c6f7ec8e90a
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-02 10:18:14 +0800

    Add files via upload

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -69,7 +69,7 @@ class FunkinLua {
 		var result:Dynamic = LuaL.dofile(lua, script);
 		var resultStr:String = Lua.tostring(lua, result);
 		if(resultStr != null && result != 0) {
-			#if windows
+			#if (windows || android)
 			lime.app.Application.current.window.alert(resultStr, 'Error on lua script!');
 			#else
 			luaTrace('Error loading lua script: "$script"\n' + resultStr,true,false);
