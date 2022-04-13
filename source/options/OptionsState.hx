cc990eb637afdd8567625760f47f3dde6422411a
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-13 21:02:07 +0800

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -118,7 +118,7 @@ class OptionsState extends MusicBeatState
 		}
 
 		if (controls.ACCEPT) {
-		    #if android
+		        #if android
 			_virtualpad.alpha = 0;
 			#end
 			openSelectedSubstate(options[curSelected]);
