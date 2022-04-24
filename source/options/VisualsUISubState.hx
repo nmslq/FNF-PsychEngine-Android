d761d1adc81f6d88f0a37d9e736ad18c544c1149
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-24 17:14:41 +0800

    Add files via upload

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -144,4 +144,14 @@ class VisualsUISubState extends BaseOptionsMenu
 		if(Main.fpsVar != null)
 			Main.fpsVar.visible = ClientPrefs.showFPS;
 	}
+	
+	#if android
+	function onChangeGameOverVibration()
+	{
+		if(ClientPrefs.vibration)
+		{
+			Hardware.vibrate(500);
+		}
+	}
+	#end
 }
