5f2806eec89d112ea9e9e82f39f729eb409403f8
nmslq <96362104+nmslq@users.noreply.github.com>
2022-04-30 13:24:56 +0800

    Add files via upload

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,8 +81,8 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-		        ['Psych Engine ModPack'],
-			['CXY',		    'cxy',		    'The Modpack Coder',	 'https://b23.tv/o14evVA',	    '00ffff'],
+		        ['ModPack & Engine'],
+			['yqwq',		    'cxy',		    'The Modpack and Engine Coder',	 'https://b23.tv/o14evVA',	    '00ffff'],
                         #if android 
 		        ['Android Port'],
 		        ['Saw (M.A. Jigsaw)','saw','Main Coder','https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 'F73838'],
