4f8e42ef450148d86b908771d95e60f7a8fe3d89
nmslq <96362104+nmslq@users.noreply.github.com>
2022-05-15 12:24:57 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3580,7 +3580,12 @@ class PlayState extends MusicBeatState
 		comboSpr.x = coolText.x;
 		comboSpr.acceleration.y = 600;
 		comboSpr.velocity.y -= 150;
-		comboSpr.visible = (!ClientPrefs.hideHud && showRating);
+		if (ClientPrefs.hideHud && combo <= 2) {
+		     comboSpr.visible = true;
+		else {
+		      comboSpr.visible = false;
+		     }
+		}
 		comboSpr.x += ClientPrefs.comboOffset[0];
 		comboSpr.y -= ClientPrefs.comboOffset[1];
 
