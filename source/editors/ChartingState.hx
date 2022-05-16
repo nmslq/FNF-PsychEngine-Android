f6dd58aba0c7e82aee18344dbfaa545d1cff8bdb
nmslq <96362104+nmslq@users.noreply.github.com>
2022-05-16 22:15:47 +0800

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1690,11 +1690,11 @@ class ChartingState extends MusicBeatState
 			
 			
 			
-			if(FlxG.keys.justPressed.Z && #if android || _virtualpad.buttonZ.justPressed #end curZoom > 0 && !FlxG.keys.pressed.CONTROL) {
+			if(FlxG.keys.justPressed.Z #if android || _virtualpad.buttonZ.justPressed #end && curZoom > 0 && !FlxG.keys.pressed.CONTROL) {
 				--curZoom;
 				updateZoom();
 			}
-			if(FlxG.keys.justPressed.X && #if android || _virtualpad.buttonC.justPressed #end curZoom < zoomList.length-1) {
+			if(FlxG.keys.justPressed.X #if android || _virtualpad.buttonC.justPressed #end && curZoom < zoomList.length-1) {
 				curZoom++;
 				updateZoom();
 			}
