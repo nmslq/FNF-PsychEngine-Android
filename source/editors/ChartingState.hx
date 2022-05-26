9a4c8c6d055accdcbb5b24ed8d641f7d3f8a7108
nmslq <96362104+nmslq@users.noreply.github.com>
2022-05-26 20:43:26 +0800

    fix

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2062,9 +2062,11 @@ class ChartingState extends MusicBeatState
 		gridBG = FlxGridOverlay.create(GRID_SIZE, GRID_SIZE, GRID_SIZE * 9, Std.int(GRID_SIZE * 32 * zoomList[curZoom]));
 		gridLayer.add(gridBG);
 
+		#if desktop
 		if(FlxG.save.data.chart_waveformInst || FlxG.save.data.chart_waveformVoices) {
 			updateWaveform();
 		}
+		#end
 
 		var gridBlack:FlxSprite = new FlxSprite(0, gridBG.height / 2).makeGraphic(Std.int(GRID_SIZE * 9), Std.int(gridBG.height / 2), FlxColor.BLACK);
 		gridBlack.alpha = 0.4;
@@ -2096,22 +2098,22 @@ class ChartingState extends MusicBeatState
 			//trace('Epic fail on the waveform lol');
 			return;
 		}
-
+		
 		wavData[0][0] = [];
 		wavData[0][1] = [];
 		wavData[1][0] = [];
 		wavData[1][1] = [];
-
+		
 		var steps:Int = _song.notes[curSection].lengthInSteps;
 		
 		var st:Float = sectionStartTime();
 		var et:Float = st + (Conductor.stepCrochet * steps * 2);
-
+		
 		if (FlxG.save.data.chart_waveformInst) {
 			var sound:FlxSound = FlxG.sound.music;
 			if (sound._sound != null && sound._sound.__buffer != null) {
 				var bytes:Bytes = sound._sound.__buffer.data.toBytes();
-
+				
 				wavData = waveformData(
 					sound._sound.__buffer,
 					bytes,
@@ -2122,12 +2124,13 @@ class ChartingState extends MusicBeatState
 					Std.int(gridBG.height)
 				);
 			}
-
+		}
+		
 		if (FlxG.save.data.chart_waveformVoices) {
 			var sound:FlxSound = vocals;
 			if (sound._sound != null && sound._sound.__buffer != null) {
 				var bytes:Bytes = sound._sound.__buffer.data.toBytes();
-
+				
 				wavData = waveformData(
 					sound._sound.__buffer,
 					bytes,
@@ -2138,102 +2141,106 @@ class ChartingState extends MusicBeatState
 					Std.int(gridBG.height)
 				);
 			}
-			
-			}
-
+		}
+		
 		// Draws
 		var gSize:Int = Std.int(GRID_SIZE * 8);
 		var hSize:Int = Std.int(gSize / 2);
-
+		
 		var lmin:Float = 0;
 		var lmax:Float = 0;
-
+		
 		var rmin:Float = 0;
 		var rmax:Float = 0;
-
+		
 		var size:Float = 1;
-
+		
 		var leftLength:Int = (
 			wavData[0][0].length > wavData[0][1].length ? wavData[0][0].length : wavData[0][1].length
 		);
-
+		
 		var rightLength:Int = (
 			wavData[1][0].length > wavData[1][1].length ? wavData[1][0].length : wavData[1][1].length
 		);
-
+		
 		var length:Int = leftLength > rightLength ? leftLength : rightLength;
-
+		
 		var index:Int;
 		for (i in 0...length) {
 			index = i;
-
+			
 			lmin = FlxMath.bound(((index < wavData[0][0].length && index >= 0) ? wavData[0][0][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
 			lmax = FlxMath.bound(((index < wavData[0][1].length && index >= 0) ? wavData[0][1][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
-
+			
 			rmin = FlxMath.bound(((index < wavData[1][0].length && index >= 0) ? wavData[1][0][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
 			rmax = FlxMath.bound(((index < wavData[1][1].length && index >= 0) ? wavData[1][1][index] : 0) * (gSize / 1.12), -hSize, hSize) / 2;
-
+			
 			waveformSprite.pixels.fillRect(new Rectangle(hSize - (lmin + rmin), i * size, (lmin + rmin) + (lmax + rmax), size), FlxColor.BLUE);
 		}
-
+		
 		waveformPrinted = true;
-		#end
 	}
 
-			function waveformData(buffer:AudioBuffer, bytes:Bytes, time:Float, endTime:Float, multiply:Float = 1,
+	/*
+	[
+		[[min...], [max...]], left
+		[[min...], [max...]]  right
+	]
+	*/
+	function waveformData(buffer:AudioBuffer, bytes:Bytes, time:Float, endTime:Float, multiply:Float = 1,
 		?array:Array<Array<Array<Float>>>, ?steps:Float):Array<Array<Array<Float>>>
 	{
 		#if (lime_cffi && !macro)
 		if (buffer == null || buffer.data == null) return [[[0], [0]], [[0], [0]]];
-
+		
 		var khz:Float = (buffer.sampleRate / 1000);
 		var channels:Int = buffer.channels;
-
+		
 		var index:Int = Std.int(time * khz);
-
+		
 		var samples:Float = ((endTime - time) * khz);
-
+		
 		if (steps == null) steps = 1280;
-
+		
 		var samplesPerRow:Float = samples / steps;
 		var samplesPerRowI:Int = Std.int(samplesPerRow);
-
+		
 		var gotIndex:Int = 0;
-
+		
 		var lmin:Float = 0;
 		var lmax:Float = 0;
-
+		
 		var rmin:Float = 0;
 		var rmax:Float = 0;
-
+		
 		var rows:Float = 0;
-
+		
 		var simpleSample:Bool = true;//samples > 17200;
 		var v1:Bool = false;
-
+		
 		if (array == null) array = [[[0], [0]], [[0], [0]]];
-
+		
 		while (index < (bytes.length - 1)) {
 			if (index >= 0) {
 				var byte:Int = bytes.getUInt16(index * channels * 2);
-
+				
 				if (byte > 65535 / 2) byte -= 65535;
-
+				
 				var sample:Float = (byte / 65535);
-
+				
 				if (sample > 0) {
 					if (sample > lmax) lmax = sample;
 				} else if (sample < 0) {
 					if (sample < lmin) lmin = sample;
 				}
-
+				
 				if (channels >= 2) {
 					byte = bytes.getUInt16((index * channels * 2) + 2);
-
+					
 					if (byte > 65535 / 2) byte -= 65535;
-
+					
 					sample = (byte / 65535);
-
+					
 					if (sample > 0) {
 						if (sample > rmax) rmax = sample;
 					} else if (sample < 0) {
@@ -2241,51 +2248,55 @@ class ChartingState extends MusicBeatState
 					}
 				}
 			}
+			
 			v1 = samplesPerRowI > 0 ? (index % samplesPerRowI == 0) : false;
 			while (simpleSample ? v1 : rows >= samplesPerRow) {
 				v1 = false;
 				rows -= samplesPerRow;
-
+				
 				gotIndex++;
-
+				
 				var lRMin:Float = Math.abs(lmin) * multiply;
 				var lRMax:Float = lmax * multiply;
-
+				
 				var rRMin:Float = Math.abs(rmin) * multiply;
 				var rRMax:Float = rmax * multiply;
-
+				
 				if (gotIndex > array[0][0].length) array[0][0].push(lRMin);
 					else array[0][0][gotIndex - 1] = array[0][0][gotIndex - 1] + lRMin;
-
+				
 				if (gotIndex > array[0][1].length) array[0][1].push(lRMax);
 					else array[0][1][gotIndex - 1] = array[0][1][gotIndex - 1] + lRMax;
-
+				
 				if (channels >= 2) {
 					if (gotIndex > array[1][0].length) array[1][0].push(rRMin);
 						else array[1][0][gotIndex - 1] = array[1][0][gotIndex - 1] + rRMin;
-
+					
 					if (gotIndex > array[1][1].length) array[1][1].push(rRMax);
 						else array[1][1][gotIndex - 1] = array[1][1][gotIndex - 1] + rRMax;
 				}
 				else {
 					if (gotIndex > array[1][0].length) array[1][0].push(lRMin);
 						else array[1][0][gotIndex - 1] = array[1][0][gotIndex - 1] + lRMin;
-
+					
 					if (gotIndex > array[1][1].length) array[1][1].push(lRMax);
 						else array[1][1][gotIndex - 1] = array[1][1][gotIndex - 1] + lRMax;
 				}
-
+				
 				lmin = 0;
 				lmax = 0;
-
+				
 				rmin = 0;
 				rmax = 0;
 			}
+			
 			index++;
 			rows++;
 			if(gotIndex > steps) break;
 		}
+		
 		return array;
+		#end
 	}
 
 	function changeNoteSustain(value:Float):Void
