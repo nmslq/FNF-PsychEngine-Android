adf58de44eb8d704804dbfa7963263ad78599062
nmslq <96362104+nmslq@users.noreply.github.com>
2022-06-05 19:28:56 +0800

    Add files via upload

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -37,7 +37,6 @@ import Character;
 import flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
 import lime.system.Clipboard;
 import flixel.animation.FlxAnimation;
-import flixel.math.FlxPoint;
 
 #if MODS_ALLOWED
 import sys.FileSystem;
@@ -149,7 +148,7 @@ class CharacterEditorState extends MusicBeatState
 
 		var tipTextArray:Array<String> = "E/Q - Camera Zoom In/Out
 		\nR - Reset Camera Zoom
-		\nMOUSE - Move Camera
+		\nJKLI - Move Camera
 		\nW/S - Previous/Next Animation
 		\nSpace - Play Animation
 		\nArrow Keys - Move Character Offset
@@ -1099,9 +1098,6 @@ class CharacterEditorState extends MusicBeatState
 		#end
 	}
 
-	private var lastPosition:FlxPoint = new FlxPoint();
-	private var mouseDiff:FlxPoint = new FlxPoint();
-
 	override function update(elapsed:Float)
 	{
 		if(char.animationsArray[curAnim] != null) {
@@ -1151,8 +1147,6 @@ class CharacterEditorState extends MusicBeatState
 			
 			if (FlxG.keys.justPressed.R #if android || _virtualpad.buttonZ.justPressed #end) {
 				FlxG.camera.zoom = 1;
-				if (FlxG.keys.pressed.SHIFT)
-					camFollow.screenCenter();
 			}
 
 			if (FlxG.keys.pressed.E #if android || _virtualpad.buttonX.pressed #end && FlxG.camera.zoom < 3) {
@@ -1164,7 +1158,7 @@ class CharacterEditorState extends MusicBeatState
 				if(FlxG.camera.zoom < 0.1) FlxG.camera.zoom = 0.1;
 			}
 
-			/*if (FlxG.keys.pressed.I || FlxG.keys.pressed.J || FlxG.keys.pressed.K || FlxG.keys.pressed.L)
+			if (FlxG.keys.pressed.I || FlxG.keys.pressed.J || FlxG.keys.pressed.K || FlxG.keys.pressed.L)
 			{
 				var addToCam:Float = 500 * elapsed;
 				if (FlxG.keys.pressed.SHIFT)
@@ -1179,7 +1173,7 @@ class CharacterEditorState extends MusicBeatState
 					camFollow.x -= addToCam;
 				else if (FlxG.keys.pressed.L)
 					camFollow.x += addToCam;
-			}*/
+			}
 
 			if(char.animationsArray.length > 0) {
 				if (FlxG.keys.justPressed.W #if android || _virtualpad.buttonV.justPressed #end)
@@ -1248,37 +1242,6 @@ class CharacterEditorState extends MusicBeatState
 		//camMenu.zoom = FlxG.camera.zoom;
 		ghostChar.setPosition(char.x, char.y);
 		super.update(elapsed);
-
-		if (FlxG.mouse.justPressedRight)
-		{
-			lastPosition.set(CoolUtil.boundTo(FlxG.mouse.getScreenPosition().x, 0, FlxG.width), 
-			CoolUtil.boundTo(FlxG.mouse.getScreenPosition().y, 0, FlxG.height));
-		}
-
-		if (FlxG.mouse.pressedRight) // draggable camera with mouse movement
-		{
-			FlxG.mouse.visible = false;
-
-			mouseDiff.set((lastPosition.x - FlxG.mouse.getScreenPosition().x), (lastPosition.y - FlxG.mouse.getScreenPosition().y));
-
-			if (FlxG.mouse.justMoved)
-			{
-				var mult:Float = 1;
-
-				if (FlxG.keys.pressed.SHIFT)
-					mult = 4;
-
-				camFollow.x = camFollow.x - -CoolUtil.boundTo(mouseDiff.x, -FlxG.width, FlxG.width) * mult;
-				camFollow.y = camFollow.y - -CoolUtil.boundTo(mouseDiff.y, -FlxG.height, FlxG.height) * mult;
-
-				lastPosition.set(CoolUtil.boundTo(FlxG.mouse.getScreenPosition().x, 0, FlxG.width), 
-				CoolUtil.boundTo(FlxG.mouse.getScreenPosition().y, 0, FlxG.height));
-			}
-		}
-		else
-		{
-			FlxG.mouse.visible = true;
-		}
 	}
 
 	var _file:FileReference;
