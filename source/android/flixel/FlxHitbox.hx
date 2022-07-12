7d9a1e33cb0cba0a7fbcb3afbed9224e7f0ba4b8
nmslq <96362104+nmslq@users.noreply.github.com>
2022-07-12 09:35:51 +0800

    fix

--- source/android/flixel/FlxHitbox.hx
+++ source/android/flixel/FlxHitbox.hx
@@ -83,7 +83,7 @@ class FlxHitbox extends FlxSpriteGroup
 	public function createHitbox(X:Float, Y:Float, Graphic:String, ?Color:Int = 0xFFFFFF):FlxButton
 	{
 		var button:FlxButton = new FlxButton(X, Y);
-		button.loadGraphic(getFrames().getByName(Graphic)));
+		button.loadGraphic(FlxGraphic.fromFrame(getFrames().getByName(Graphic)));
 		button.setGraphicSize(Std.int(FlxG.width / 4), FlxG.height);
 		button.updateHitbox();
 		button.color = Color;
