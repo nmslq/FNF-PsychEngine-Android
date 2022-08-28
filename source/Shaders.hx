db634cfd6717de83b28200a5ddd9786af1b540c4
nmslq <96362104+nmslq@users.noreply.github.com>
2022-08-28 13:29:35 +0800

    codebase

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -91,9 +91,9 @@ class ChromaticAberrationEffect extends Effect
 	public function new(offset:Float = 0.00)
 	{
 		shader = new ChromaticAberrationShader();
-    	shader.rOffset.value = [offset];
-    	shader.gOffset.value = [0.0];
-    	shader.bOffset.value = [-offset];
+		shader.rOffset.value = [offset];
+		shader.gOffset.value = [0.0];
+		shader.bOffset.value = [-offset];
 	}
 
 	public function setChrome(chromeOffset:Float):Void
@@ -159,128 +159,79 @@ class Tiltshift extends FlxShader
 {
 	@:glFragmentSource('
 		#pragma header
-
-		// Modified version of a tilt shift shader from Martin Jonasson (http://grapefrukt.com/)
-		// Read http://notes.underscorediscovery.com/ for context on shaders and this file
-		// License : MIT
-		 
-			/*
-				Take note that blurring in a single pass (the two for loops below) is more expensive than separating
-				the x and the y blur into different passes. This was used where bleeding edge performance
-				was not crucial and is to illustrate a point. 
-		 
-				The reason two passes is cheaper? 
-				   texture2D is a fairly high cost call, sampling a texture.
-		 
-				   So, in a single pass, like below, there are 3 steps, per x and y. 
-		 
-				   That means a total of 9 "taps", it touches the texture to sample 9 times.
-		 
-				   Now imagine we apply this to some geometry, that is equal to 16 pixels on screen (tiny)
-				   (16 * 16) * 9 = 2304 samples taken, for width * height number of pixels, * 9 taps
-				   Now, if you split them up, it becomes 3 for x, and 3 for y, a total of 6 taps
-				   (16 * 16) * 6 = 1536 samples
-			
-				   That\'s on a *tiny* sprite, let\'s scale that up to 128x128 sprite...
-				   (128 * 128) * 9 = 147,456
-				   (128 * 128) * 6 =  98,304
-		 
-				   That\'s 33.33..% cheaper for splitting them up.
-				   That\'s with 3 steps, with higher steps (more taps per pass...)
-		 
-				   A really smooth, 6 steps, 6*6 = 36 taps for one pass, 12 taps for two pass
-				   You will notice, the curve is not linear, at 12 steps it\'s 144 vs 24 taps
-				   It becomes orders of magnitude slower to do single pass!
-				   Therefore, you split them up into two passes, one for x, one for y.
-			*/
-		 
-		// I am hardcoding the constants like a jerk
-			
 		uniform float bluramount;
 		uniform float center;
 		const float stepSize    = 0.004;
 		const float steps       = 3.0;
-		 
+
 		const float minOffs     = (float(steps-1.0)) / -2.0;
 		const float maxOffs     = (float(steps-1.0)) / +2.0;
-		 
+
 		void main() {
 			float amount;
 			vec4 blurred;
-				
-			// Work out how much to blur based on the mid point 
+
 			amount = pow((openfl_TextureCoordv.y * center) * 2.0 - 1.0, 2.0) * bluramount;
-				
-			// This is the accumulation of color from the surrounding pixels in the texture
+
 			blurred = vec4(0.0, 0.0, 0.0, 1.0);
-				
-			// From minimum offset to maximum offset
+
 			for (float offsX = minOffs; offsX <= maxOffs; ++offsX) {
 				for (float offsY = minOffs; offsY <= maxOffs; ++offsY) {
-		 
-					// copy the coord so we can mess with it
+
 					vec2 temp_tcoord = openfl_TextureCoordv.xy;
-		 
-					//work out which uv we want to sample now
+
 					temp_tcoord.x += offsX * amount * stepSize;
 					temp_tcoord.y += offsY * amount * stepSize;
-		 
-					// accumulate the sample 
+
 					blurred += texture2D(bitmap, temp_tcoord);
 				}
 			} 
-				
-			// because we are doing an average, we divide by the amount (x AND y, hence steps * steps)
+
 			blurred /= float(steps * steps);
-		 
-			// return the final blurred color
+
 			gl_FragColor = blurred;
-		}')
+		}
+	')
+
 	public function new()
 	{
 		super();
 	}
 }
-class GreyscaleEffect extends Effect{
-	
+
+class GreyscaleEffect extends Effect
+{
 	public var shader:GreyscaleShader = new GreyscaleShader();
-	
-	public function new(){
-		
+
+	public function new()
+	{
+
 	}
-	
-	
 }
-class GreyscaleShader extends FlxShader{
+
+class GreyscaleShader extends FlxShader
+{
 	@:glFragmentSource('
 	#pragma header
-	void main() {
-		vec4 color = texture2D(bitmap, openfl_TextureCoordv);
-		float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
-		gl_FragColor = vec4(vec3(gray), color.a);
-	}
-	
-	
+		void main() {
+			vec4 color = texture2D(bitmap, openfl_TextureCoordv);
+			float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+			gl_FragColor = vec4(vec3(gray), color.a);
+		}
 	')
-	
-	public function new(){
+
+	public function new()
+	{
 		super();
 	}
-	
-	
-	
 }
 
-
-
-
-
-
-
-class GrainEffect extends Effect {
-	
+class GrainEffect extends Effect
+{
 	public var shader:Grain;
-	public function new (grainsize, lumamount,lockAlpha,coloramount){
+
+	public function new (grainsize, lumamount,lockAlpha,coloramount)
+	{
 		shader = new Grain();
 		shader.lumamount.value = [lumamount];
 		shader.grainsize.value = [grainsize];
@@ -289,54 +240,30 @@ class GrainEffect extends Effect {
 		shader.uTime.value = [FlxG.random.float(0,8)];
 		PlayState.instance.shaderUpdates.push(update);
 	}
-	public function update(elapsed){
+	public function update(elapsed)
+	{
 		shader.uTime.value[0] += elapsed;
 	}
-	
-	
-	
-	
 }
-
 
 class Grain extends FlxShader
 {
 	@:glFragmentSource('
 		#pragma header
-
-		/*
-		Film Grain post-process shader v1.1
-		Martins Upitis (martinsh) devlog-martinsh.blogspot.com
-		2013
-
-		--------------------------
-		This work is licensed under a Creative Commons Attribution 3.0 Unported License.
-		So you are free to share, modify and adapt it for your needs, and even use it for commercial use.
-		I would also love to hear about a project you are using it.
-
-		Have fun,
-		Martins
-		--------------------------
-
-		Perlin noise shader by toneburst:
-		http://machinesdontcare.wordpress.com/2009/06/25/3d-perlin-noise-sphere-vertex-shader-sourcecode/
-		*/
 		uniform float uTime;
 
-		const float permTexUnit = 1.0/256.0;        // Perm texture texel-size
-		const float permTexUnitHalf = 0.5/256.0;    // Half perm texture texel-size
+		const float permTexUnit = 1.0/256.0;
+		const float permTexUnitHalf = 0.5/256.0;
 
 		float width = openfl_TextureSize.x;
 		float height = openfl_TextureSize.y;
 
-		const float grainamount = 0.05; //grain amount
-		bool colored = false; //colored noise?
+		const float grainamount = 0.05;
+		bool colored = false;
 		uniform float coloramount;
-		uniform float grainsize; //grain particle size (1.5 - 2.5)
-		uniform float lumamount; //
-	uniform bool lockAlpha;
-
-		//a random texture generator, but you can also use a pre-computed perturbation texture
+		uniform float grainsize;
+		uniform float lumamount;
+		uniform bool lockAlpha;
 	
 		vec4 rnm(in vec2 tc)
 		{
@@ -358,52 +285,43 @@ class Grain extends FlxShader
 
 		float pnoise3D(in vec3 p)
 		{
-			vec3 pi = permTexUnit*floor(p)+permTexUnitHalf; // Integer part, scaled so +1 moves permTexUnit texel
-			// and offset 1/2 texel to sample texel centers
-			vec3 pf = fract(p);     // Fractional part for interpolation
+			vec3 pi = permTexUnit*floor(p)+permTexUnitHalf;
+			vec3 pf = fract(p);
 
-			// Noise contributions from (x=0, y=0), z=0 and z=1
 			float perm00 = rnm(pi.xy).a ;
 			vec3  grad000 = rnm(vec2(perm00, pi.z)).rgb * 4.0 - 1.0;
 			float n000 = dot(grad000, pf);
 			vec3  grad001 = rnm(vec2(perm00, pi.z + permTexUnit)).rgb * 4.0 - 1.0;
 			float n001 = dot(grad001, pf - vec3(0.0, 0.0, 1.0));
 
-			// Noise contributions from (x=0, y=1), z=0 and z=1
 			float perm01 = rnm(pi.xy + vec2(0.0, permTexUnit)).a ;
 			vec3  grad010 = rnm(vec2(perm01, pi.z)).rgb * 4.0 - 1.0;
 			float n010 = dot(grad010, pf - vec3(0.0, 1.0, 0.0));
 			vec3  grad011 = rnm(vec2(perm01, pi.z + permTexUnit)).rgb * 4.0 - 1.0;
 			float n011 = dot(grad011, pf - vec3(0.0, 1.0, 1.0));
 
-			// Noise contributions from (x=1, y=0), z=0 and z=1
 			float perm10 = rnm(pi.xy + vec2(permTexUnit, 0.0)).a ;
 			vec3  grad100 = rnm(vec2(perm10, pi.z)).rgb * 4.0 - 1.0;
 			float n100 = dot(grad100, pf - vec3(1.0, 0.0, 0.0));
 			vec3  grad101 = rnm(vec2(perm10, pi.z + permTexUnit)).rgb * 4.0 - 1.0;
 			float n101 = dot(grad101, pf - vec3(1.0, 0.0, 1.0));
 
-			// Noise contributions from (x=1, y=1), z=0 and z=1
 			float perm11 = rnm(pi.xy + vec2(permTexUnit, permTexUnit)).a ;
 			vec3  grad110 = rnm(vec2(perm11, pi.z)).rgb * 4.0 - 1.0;
 			float n110 = dot(grad110, pf - vec3(1.0, 1.0, 0.0));
 			vec3  grad111 = rnm(vec2(perm11, pi.z + permTexUnit)).rgb * 4.0 - 1.0;
 			float n111 = dot(grad111, pf - vec3(1.0, 1.0, 1.0));
 
-			// Blend contributions along x
 			vec4 n_x = mix(vec4(n000, n001, n010, n011), vec4(n100, n101, n110, n111), fade(pf.x));
 
-			// Blend contributions along y
 			vec2 n_xy = mix(n_x.xy, n_x.zw, fade(pf.y));
 
-			// Blend contributions along z
 			float n_xyz = mix(n_xy.x, n_xy.y, fade(pf.z));
 
 			// We are done, return the final noise value.
 			return n_xyz;
 		}
 
-		//2d coordinate orientation thing
 		vec2 coordRot(in vec2 tc, in float angle)
 		{
 			float aspect = width/height;
@@ -432,7 +350,6 @@ class Grain extends FlxShader
 
 			vec3 col = texture2D(bitmap, openfl_TextureCoordv).rgb;
 
-			//noisiness response curve based on scene luminance
 			vec3 lumcoeff = vec3(0.299,0.587,0.114);
 			float luminance = mix(0.0,dot(col, lumcoeff),lumamount);
 			float lum = smoothstep(0.2,0.0,luminance);
@@ -446,60 +363,66 @@ class Grain extends FlxShader
 			vec4 texColor = texture2D(bitmap, openfl_TextureCoordv);
 				if (lockAlpha) bitch = texColor.a;
 			gl_FragColor =  vec4(col,bitch);
-		}')
+		}
+	')
+
 	public function new()
 	{
 		super();
 	}
-	
-	
 }
 
 class VCRDistortionEffect extends Effect
 {
-  public var shader:VCRDistortionShader = new VCRDistortionShader();
-  public function new(glitchFactor:Float,distortion:Bool=true,perspectiveOn:Bool=true,vignetteMoving:Bool=true){
-    shader.iTime.value = [0];
-    shader.vignetteOn.value = [true];
-    shader.perspectiveOn.value = [perspectiveOn];
-    shader.distortionOn.value = [distortion];
-    shader.scanlinesOn.value = [true];
-    shader.vignetteMoving.value = [vignetteMoving];
-    shader.glitchModifier.value = [glitchFactor];
-    shader.iResolution.value = [Lib.current.stage.stageWidth,Lib.current.stage.stageHeight];
-   // var noise = Assets.getBitmapData(Paths.image("noise2"));
-   // shader.noiseTex.input = noise;
-   PlayState.instance.shaderUpdates.push(update);
-  }
+	public var shader:VCRDistortionShader = new VCRDistortionShader();
+	public function new(glitchFactor:Float,distortion:Bool=true,perspectiveOn:Bool=true,vignetteMoving:Bool=true)
+	{
+    	shader.iTime.value = [0];
+    	shader.vignetteOn.value = [true];
+    	shader.perspectiveOn.value = [perspectiveOn];
+    	shader.distortionOn.value = [distortion];
+    	shader.scanlinesOn.value = [true];
+    	shader.vignetteMoving.value = [vignetteMoving];
+    	shader.glitchModifier.value = [glitchFactor];
+    	shader.iResolution.value = [Lib.current.stage.stageWidth,Lib.current.stage.stageHeight];
+    	PlayState.instance.shaderUpdates.push(update);
+	}
 
-  public function update(elapsed:Float){
-    shader.iTime.value[0] += elapsed;
-    shader.iResolution.value = [Lib.current.stage.stageWidth,Lib.current.stage.stageHeight];
-  }
+	public function update(elapsed:Float)
+	{
+    	shader.iTime.value[0] += elapsed;
+    	shader.iResolution.value = [Lib.current.stage.stageWidth,Lib.current.stage.stageHeight];
+	}
 
-  public function setVignette(state:Bool){
-    shader.vignetteOn.value[0] = state;
-  }
+	public function setVignette(state:Bool)
+	{
+    	shader.vignetteOn.value[0] = state;
+	}
 
-  public function setPerspective(state:Bool){
-    shader.perspectiveOn.value[0] = state;
-  }
+	public function setPerspective(state:Bool)
+	{
+    	shader.perspectiveOn.value[0] = state;
+	}
 
-  public function setGlitchModifier(modifier:Float){
-    shader.glitchModifier.value[0] = modifier;
-  }
+	public function setGlitchModifier(modifier:Float)
+	{
+    	shader.glitchModifier.value[0] = modifier;
+	}
 
-  public function setDistortion(state:Bool){
-    shader.distortionOn.value[0] = state;
-  }
+	public function setDistortion(state:Bool)
+	{
+    	shader.distortionOn.value[0] = state;
+	}
 
-  public function setScanlines(state:Bool){
-    shader.scanlinesOn.value[0] = state;
-  }
+	public function setScanlines(state:Bool)
+	{
+    	shader.scanlinesOn.value[0] = state;
+	}
 
-  public function setVignetteMoving(state:Bool){
-    shader.vignetteMoving.value[0] = state;
-  }
+	public function setVignetteMoving(state:Bool)
+	{
+    	shader.vignetteMoving.value[0] = state;
+	}
 }
 
 class VCRDistortionShader extends FlxShader // https://www.shadertoy.com/view/ldjGzV and https://www.shadertoy.com/view/Ms23DR and https://www.shadertoy.com/view/MsXGD4 and https://www.shadertoy.com/view/Xtccz4
