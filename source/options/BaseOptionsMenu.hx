85bd87186cb5429048907fe6afee7225b4c3948d
nmslq <96362104+nmslq@users.noreply.github.com>
2022-08-31 13:36:37 +0800

    fixes

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -89,7 +89,7 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 		for (i in 0...optionsArray.length)
 		{
-			var optionText:Alphabet = new Alphabet(290, 260, optionsArray[i].name, false, false);
+			var optionText:Alphabet = new Alphabet(290, 260, optionsArray[i].name, false);
 			optionText.isMenuItem = true;
 			/*optionText.forceX = 300;
 			optionText.yMult = 90;*/
