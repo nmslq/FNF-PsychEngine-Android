3a65525bf981f8f7d346e781196f30b1b184e159
nmslq <96362104+nmslq@users.noreply.github.com>
2022-09-10 09:47:22 +0800

    maybe blur effect

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -632,6 +632,74 @@ class VCRDistortionShader extends FlxShader // https://www.shadertoy.com/view/ld
 	}
 }
 
+class BlurEffect extends Effect
+{
+	public var shader:BlurShader = new BlurShader();
+
+	public function new(?size:Float = 18.0, ?qualitly:Float = 8.0, ?dim:Float = 1.8, ?directions:Float = 16.0)
+	{
+		shader.Size.value = [size];
+		shader.Quality.value = [qualitly];
+		shader.dim.value = [dim];
+		shader.Directions.value = [directions];
+	}
+
+	public function update(elapsed:Float)
+	{
+	}
+
+	public function setSize(modifier:Float)
+	{
+		shader.Size.value = [modifier];
+	}
+
+	public function setDim(mod:Float)
+	{
+		shader.dim.value = [mod];
+	}
+}
+
+class BlurShader extends FlxShader // BLOOM SHADER BY BBPANZU
+{
+	@:glFragmentSource('
+	#pragma header
+
+    // GAUSSIAN BLUR SETTINGS
+  	uniform float dim;
+    uniform float Directions;
+    uniform float Quality; 
+    uniform float Size; 
+
+	void main(void)
+	{ 
+		vec2 uv = openfl_TextureCoordv.xy ;
+
+		float Pi = 6.28318530718; // Pi*2
+
+		vec4 Color = texture2D( bitmap, uv);
+		
+		for(float d=0.0; d<Pi; d+=Pi/Directions){
+			for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality){
+
+				float ex = (cos(d)*Size*i)/openfl_TextureSize.x;
+				float why = (sin(d)*Size*i)/openfl_TextureSize.y;
+				Color += flixel_texture2D( bitmap, uv+vec2(ex,why));	
+			}
+		}
+		
+		Color /= (dim * Quality) * Directions - 15.0;
+		vec4 bloom =  (flixel_texture2D( bitmap, uv)/ dim)+Color;
+
+		gl_FragColor = bloom;
+
+	}
+	')
+	public function new()
+	{
+		super();
+	}
+}
+
 class DistortionEffect extends Effect
 {
 	public var shader:DistortionShader = new DistortionShader();
