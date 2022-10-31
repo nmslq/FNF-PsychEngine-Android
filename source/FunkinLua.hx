a6f7e6293e231e27fb5c0486bc0482e0dbb8424e
nmslq <96362104+nmslq@users.noreply.github.com>
2022-10-31 22:16:54 +0800

    fix lua??

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -639,18 +639,25 @@ class FunkinLua {
 					if(luaInstance.scriptName == cervix)
 					{
 						Lua.getglobal(luaInstance.lua, global);
-						if(Lua.isnumber(luaInstance.lua,-1)){
+						if(Lua.isnumber(luaInstance.lua,-1))
+						{
 							Lua.pushnumber(lua, Lua.tonumber(luaInstance.lua, -1));
-						}else if(Lua.isstring(luaInstance.lua,-1)){
+						}
+						else if(Lua.isstring(luaInstance.lua,-1))
+						{
 							Lua.pushstring(lua, Lua.tostring(luaInstance.lua, -1));
-						}else if(Lua.isboolean(luaInstance.lua,-1)){
-							Lua.pushboolean(lua, Lua.toboolean(luaInstance.lua, -1));
-						}else{
+						}
+						else if(Lua.isinteger(luaInstance.lua,-1))
+						{
+							Lua.pushinteger(lua, Lua.integer(luaInstance.lua, -1));
+						}
+						else
+						{
 							Lua.pushnil(lua);
 						}
 						// TODO: table
 
-						Lua.pop(luaInstance.lua,1); // remove the global
+						Lua.pop(luaInstance.lua, 1); // remove the global
 
 						return;
 					}
