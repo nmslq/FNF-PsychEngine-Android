efc9fbfabfe2ee2d3fb2b98bf83431ca1e0f83f0
nmslq <96362104+nmslq@users.noreply.github.com>
2023-03-05 11:10:25 +0800

    new

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -18,6 +18,7 @@ typedef AchievementMeta = {
 	public var desc:String;
 	public var save_tag:String;
 	public var hidden:Bool;
+	public var ?song:String;
 
 	public var ?week_nomiss:String;
 	public var ?lua_code:String;
@@ -181,7 +182,7 @@ class Achievements {
 
 	public static function loadAchievements():Void
 	{
-		#if MODS_ALLOWED
+		#if (MODS_ALLOWED && ACHIEVEMENTS_ALLOWED)
 		loadModAchievements();
 		#end
 
@@ -198,48 +199,41 @@ class Achievements {
 		}
 	}
 
-	#if MODS_ALLOWED
-	public static function loadModAchievements()
-	{
+	#if (MODS_ALLOWED && ACHIEVEMENTS_ALLOWED)
+	public static function loadModAchievements() {
 		achievementsStuff = copyAchievements.copy();
-
+		var oldPath:Array<String> = Paths.globalMods.copy();
+		Paths.globalMods = [];
 		var paths:Array<String>= [Paths.modFolders('achievements/'),Paths.getPreloadPath('achievements/'),];
-		for(i in paths.copy())
-		{
-			if(FileSystem.exists(i))
-			{
-				for(l in FileSystem.readDirectory(i))
-				{
-					if(l.endsWith('.json'))
-					{
+		Paths.globalMods = oldPath;
+		for(i in paths.copy()){
+			if(FileSystem.exists(i)){
+				for(l in FileSystem.readDirectory(i)){
+					if(l.endsWith('.json')){
 						var meta:AchievementMeta = cast haxe.Json.parse(File.getContent(i + l));
-						if(meta != null)
-						{
-							if(meta.clearAchievements)
-								achievementsStuff = [];
+						if(meta!=null){
+							if (meta.global != null && meta.global.length > 0 && !FileSystem.exists(i + l.substring(0, l.length - 4) + 'lua'))
+								throw "(" + l + ") global needs a lua file to work.\nCreate a lua file named \"" + l.substring(0, l.length - 5) + "\" in \"" + i + "\".";
 
-							if(meta.global == null || meta.global.length < 1)
-							{
+							if(meta.global==null||meta.global.length<1){
+								if(meta.clearAchievements)
+									achievementsStuff=[];
 								var achievement:Array<Dynamic> = [];
 								achievement.push(meta.name);
 								achievement.push(meta.desc);
 								achievement.push(meta.save_tag);
 								achievement.push(meta.hidden);
 								var index:Null<Int> = meta.index;
-								if(!achievementsStuff.contains(achievement))
-								{
-									if(index == null || index < 0)
-									{
+								if(!achievementsStuff.contains(achievement)) {
+									if(index==null||index<0){
 										achievementsStuff.push(achievement.copy());
 									}
-									else
-									{
+									else {
 										achievementsStuff.insert(index,achievement);
 									}
 								}
 							}
-							else
-							{
+							else{
 								achievementsStuff = meta.global.copy();
 							}
 						}
@@ -249,23 +243,20 @@ class Achievements {
 		}
 	}
 
-	public static function getModAchievements():Array<String>
-	{
+	public static function getModAchievements():Array<String> {
+		var oldPath:Array<String> = Paths.globalMods.copy();
+		Paths.globalMods = [];
 		var paths:Array<String>= [Paths.modFolders('achievements/'),Paths.getPreloadPath('achievements/'),];
-
+		Paths.globalMods = oldPath;
 		var luas:Array<String> = [];
-		for(i in paths)
-		{
-			if(FileSystem.exists(i))
-			{
-				for(l in FileSystem.readDirectory(i))
-				{
+		for(i in paths){
+			if(FileSystem.exists(i)){
+				for(l in FileSystem.readDirectory(i)){
 					var pushedLuas = [];
 					var file = l.substr(0, l.length - 4);
 					//ignore lua files that does not have a json file
-					if (l.endsWith('.lua') && FileSystem.exists(i+file+'.json') && !pushedLuas.contains(l))
-					{
-						luas.push(i + l);
+					if (l.endsWith('.lua') && FileSystem.exists(i+file+'.json') && !pushedLuas.contains(l)) {
+						luas.push(i+l);
 						pushedLuas.push(l);
 					}
 				}
@@ -274,19 +265,19 @@ class Achievements {
 		return luas.copy();
 	}
 
-	public static function getModAchievementMetas():Array<AchievementMeta>
-	{
+	public static function getModAchievementMetas():Array<AchievementMeta> {
+		var oldPath:Array<String> = Paths.globalMods.copy();
+		Paths.globalMods = [];
 		var paths:Array<String>= [Paths.modFolders('achievements/'),Paths.getPreloadPath('achievements/'),];
-
+		Paths.globalMods = oldPath;
 		var metas = [];
-		for(i in paths.copy())
+		for(i in paths)
 			if(FileSystem.exists(i))
 				for(l in FileSystem.readDirectory(i))
 					if(l.endsWith('.json'))
 					{
-						try
-						{
-							var meta:AchievementMeta = cast haxe.Json.parse(File.getContent(i + l));
+						try {
+							var meta:AchievementMeta = haxe.Json.parse(File.getContent(i + l));
 							metas.push(meta);
 						}
 						catch(e) {
@@ -294,7 +285,7 @@ class Achievements {
 						}
 					}
 
-		return metas.copy();
+		return metas;
 	}
 	#end
 }
