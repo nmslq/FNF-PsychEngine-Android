4660f6322604f0275393a2e3e8ba7b2392ce7d2d
nmslq <96362104+nmslq@users.noreply.github.com>
2023-03-29 18:17:49 +0800

    Delete GithubLoad.hx

--- source/GithubLoad.hx
+++
@@ -1,8 +0,0 @@
-package;
-
-class GithubLoad
-{
-    public static var repoHolder:String = 'Shadow-Mario';
-    public static var repoName:String = 'FNF-PsychEngine';
-    public static var personalAccessToken:String = '';
-}
