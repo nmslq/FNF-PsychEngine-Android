50928e1c3f3c8e4df4d9ab17bb34d1b7f882aa15
nmslq <96362104+nmslq@users.noreply.github.com>
2023-05-03 12:48:01 +0800

    Add files via upload

--- source/editors/EditorPlayState.hx
+++ source/editors/EditorPlayState.hx
@@ -22,7 +22,7 @@ import flixel.util.FlxSort;
 import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
 import openfl.events.KeyboardEvent;
-import FunkinLua;
+import psychlua.FunkinLua;
 
 using StringTools;
 
