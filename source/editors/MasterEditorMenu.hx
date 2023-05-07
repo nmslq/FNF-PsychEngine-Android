eb8ce86242c50db85b6c5af6231db169fcb7f002
nmslq <96362104+nmslq@users.noreply.github.com>
2023-05-07 20:26:21 +0800

    Add files via upload

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -5,20 +5,16 @@ import Discord.DiscordClient;
 #end
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.addons.display.FlxGridOverlay;
-import flixel.addons.transition.FlxTransitionableState;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-#if (flixel >= "5.3.0")
-import flixel.sound.FlxSound;
-#else
-import flixel.system.FlxSound;
-#end
+
 #if MODS_ALLOWED
 import sys.FileSystem;
 #end
+
+import menus.MainMenuState;
+import menus.FreeplayState;
 
 using StringTools;
 
