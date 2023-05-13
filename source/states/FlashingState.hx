575b1b099ab1e3042cf631e65666c7e60615aa06
nmslq <96362104+nmslq@users.noreply.github.com>
2023-05-13 11:35:14 +0800

    Update and rename source/FlashingState.hx to source/states/FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -1,15 +1,9 @@
-package;
+package states;
 
-import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.FlxSubState;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
 import flixel.effects.FlxFlicker;
 import lime.app.Application;
 import flixel.addons.transition.FlxTransitionableState;
-import flixel.tweens.FlxTween;
-import flixel.util.FlxTimer;
 
 class FlashingState extends MusicBeatState
 {
