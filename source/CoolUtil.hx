c0baae3d29f98a8f962b0581eee0c28e1c10d3c8
nmslq <96362104+nmslq@users.noreply.github.com>
2023-05-27 15:10:36 +0800

    Add files via upload

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -1,37 +1,36 @@
 package;
 
 import flixel.util.FlxSave;
-#if MODS_ALLOWED
+
+import openfl.utils.Assets;
+import lime.utils.Assets as LimeAssets;
+
+#if sys
 import sys.io.File;
 import sys.FileSystem;
-#else
-import openfl.utils.Assets;
 #end
 
 class CoolUtil
 {
-	inline public static function quantize(f:Float, snap:Float)
-	{
+	inline public static function quantize(f:Float, snap:Float){
 		// changed so this actually works lol
 		var m:Float = Math.fround(f * snap);
 		trace(snap);
 		return (m / snap);
 	}
 
-	inline public static function boundTo(value:Float, min:Float, max:Float):Float
+	inline public static function capitalize(text:String)
 	{
-		return Math.max(min, Math.min(max, value));
+		return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
 	}
 
-	public static function coolTextFile(path:String):Array<String>
+	inline public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if MODS_ALLOWED
-		if (FileSystem.exists(path))
-			daList = File.getContent(path).trim().split('\n');
+		#if sys
+		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
 		#else
-		if (Assets.exists(path))
-			daList = Assets.getText(path).trim().split('\n');
+		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
 		#end
 
 		for (i in 0...daList.length)
@@ -42,7 +41,7 @@ class CoolUtil
 		return daList;
 	}
 
-	public static function listFromString(string:String):Array<String>
+	inline public static function listFromString(string:String):Array<String>
 	{
 		var daList:Array<String> = [];
 		daList = string.trim().split('\n');
@@ -54,65 +53,53 @@ class CoolUtil
 
 		return daList;
 	}
-
-	public static function dominantColor(sprite:flixel.FlxSprite):Int
+	
+	inline public static function dominantColor(sprite:flixel.FlxSprite):Int
 	{
 		var countByColor:Map<Int, Int> = [];
-		for (col in 0...sprite.frameWidth)
-		{
-			for (row in 0...sprite.frameHeight)
-			{
+		for(col in 0...sprite.frameWidth) {
+			for(row in 0...sprite.frameHeight) {
 				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-				if (colorOfThisPixel != 0)
-				{
-					if (countByColor.exists(colorOfThisPixel))
-					{
+				if(colorOfThisPixel != 0) {
+					if(countByColor.exists(colorOfThisPixel))
 						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
-					}
-					else if (countByColor[colorOfThisPixel] != 13520687 - (2 * 13520687))
-					{
+					else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687))
 						countByColor[colorOfThisPixel] = 1;
-					}
 				}
 			}
 		}
+
 		var maxCount = 0;
-		var maxKey:Int = 0; // after the loop this will store the max color
-		countByColor[flixel.util.FlxColor.BLACK] = 0;
-			for(key in countByColor.keys())
-			{
-			if (countByColor[key] >= maxCount)
-			{
+		var maxKey:Int = 0; //after the loop this will store the max color
+		countByColor[FlxColor.BLACK] = 0;
+		for(key in countByColor.keys()) {
+			if(countByColor[key] >= maxCount) {
 				maxCount = countByColor[key];
 				maxKey = key;
 			}
 		}
+		countByColor = [];
 		return maxKey;
 	}
 
-	public static function numberArray(max:Int, ?min = 0):Array<Int>
+	inline public static function numberArray(max:Int, ?min = 0):Array<Int>
 	{
 		var dumbArray:Array<Int> = [];
-		for (i in min...max)
-		{
-			dumbArray.push(i);
-		}
+		for (i in min...max) dumbArray.push(i);
+
 		return dumbArray;
 	}
 
 	//uhhhh does this even work at all? i'm starting to doubt
-	public static function precacheSound(sound:String, ?library:String = null):Void
-	{
+	inline public static function precacheSound(sound:String, ?library:String = null):Void {
 		Paths.sound(sound, library);
 	}
 
-	public static function precacheMusic(sound:String, ?library:String = null):Void
-	{
+	inline public static function precacheMusic(sound:String, ?library:String = null):Void {
 		Paths.music(sound, library);
 	}
 
-	public static function browserLoad(site:String)
-	{
+	inline public static function browserLoad(site:String) {
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
 		#else
@@ -125,7 +112,7 @@ class CoolUtil
 		so Base Psych saves won't conflict with yours
 		@BeastlyGabi
 	**/
-	public static function getSavePath(folder:String = 'ShadowMario'):String {
+	inline public static function getSavePath(folder:String = 'ShadowMario'):String {
 		@:privateAccess
 		return #if (flixel < "5.0.0") folder #else FlxG.stage.application.meta.get('company')
 			+ '/'
