41af3cd41a86806f3b251e4ad8d51f2cfa3491c7
nmslq <96362104+nmslq@users.noreply.github.com>
2023-06-04 13:32:25 +0800

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -93,12 +93,7 @@ class Main extends Sprite
 		#end
 
 		#if desktop
-		if (!DiscordClient.isInitialized) {
-			DiscordClient.initialize();
-			Application.current.window.onClose.add(function() {
-				DiscordClient.shutdown();
-			});
-		}
+		DiscordClient.start();
 		#end
 	}
 
