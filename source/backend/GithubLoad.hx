b82374b449098d21e4435b2f748ce0a04c421a1c
nmslq <96362104+nmslq@users.noreply.github.com>
2023-06-06 19:57:35 +0800

    Add files via upload

---
+++ source/backend/GithubLoad.hx
@@ -0,0 +1,8 @@
+package backend;
+
+class GithubLoad
+{
+    public static var repoHolder:String = 'Shadow-Mario';
+    public static var repoName:String = 'FNF-PsychEngine';
+    public static var personalAccessToken:String = '';
+}
