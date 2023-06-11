c0f0d838d6941022ef7656f239f3c2d5f325615f
nmslq <96362104+nmslq@users.noreply.github.com>
2023-06-11 17:42:12 +0800

    Add files via upload

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -158,6 +158,8 @@ class ClientPrefs {
 		if (FlxG.save.data.mute != null)
 			FlxG.sound.muted = FlxG.save.data.mute;
 
+		FlxSprite.defaultAntialiasing = data.antialiasing;
+
 		#if desktop
 		DiscordClient.check();
 		#end
