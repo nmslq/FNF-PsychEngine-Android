e8f94e2ba7884a3b66c3d5e90e0a3cb6b0fc370c
nmslq <96362104+nmslq@users.noreply.github.com>
2023-06-11 14:33:31 +0800

    Add files via upload

--- source/backend/Achievements.hx
+++ source/backend/Achievements.hx
@@ -194,10 +194,10 @@ class Achievements {
 	#if (MODS_ALLOWED && ACHIEVEMENTS_ALLOWED)
 	public static function loadModAchievements() {
 		achievementsStuff = copyAchievements.copy();
-		var oldPath:Array<String> = Paths.globalMods.copy();
-		Paths.globalMods = [];
+		var oldPath:Array<String> = Mods.globalMods.copy();
+		Mods.globalMods = [];
 		var paths:Array<String>= [Paths.modFolders('achievements/'),Paths.getPreloadPath('achievements/'),];
-		Paths.globalMods = oldPath;
+		Mods.globalMods = oldPath;
 		for(i in paths.copy()){
 			if(FileSystem.exists(i)){
 				for(l in FileSystem.readDirectory(i)){
@@ -236,10 +236,10 @@ class Achievements {
 	}
 
 	public static function getModAchievements():Array<String> {
-		var oldPath:Array<String> = Paths.globalMods.copy();
-		Paths.globalMods = [];
+		var oldPath:Array<String> = Mods.globalMods.copy();
+		Mods.globalMods = [];
 		var paths:Array<String>= [Paths.modFolders('achievements/'),Paths.getPreloadPath('achievements/'),];
-		Paths.globalMods = oldPath;
+		Mods.globalMods = oldPath;
 		var luas:Array<String> = [];
 		for(i in paths){
 			if(FileSystem.exists(i)){
@@ -258,10 +258,10 @@ class Achievements {
 	}
 
 	public static function getModAchievementMetas():Array<AchievementMeta> {
-		var oldPath:Array<String> = Paths.globalMods.copy();
-		Paths.globalMods = [];
+		var oldPath:Array<String> = Mods.globalMods.copy();
+		Mods.globalMods = [];
 		var paths:Array<String>= [Paths.modFolders('achievements/'),Paths.getPreloadPath('achievements/'),];
-		Paths.globalMods = oldPath;
+		Mods.globalMods = oldPath;
 		var metas = [];
 		for(i in paths)
 			if(FileSystem.exists(i))
