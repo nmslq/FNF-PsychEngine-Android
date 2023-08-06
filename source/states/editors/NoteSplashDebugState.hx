825f74d86ab3d4f0e734de44d2e6eecef4b79de4
nmslq <96362104+nmslq@users.noreply.github.com>
2023-08-06 14:23:24 +0800

    fix

--- source/states/editors/NoteSplashDebugState.hx
+++ source/states/editors/NoteSplashDebugState.hx
@@ -109,7 +109,7 @@ class NoteSplashDebugState extends MusicBeatState
 		var text:FlxText = new FlxText(0, 520, FlxG.width,
 			"Press B to Reset animation\n
 			Press A twice to save to the loaded Note Splash PNG's folder\n
-			Left/Right change selected note - Arrow Keys to change offset (Hold shift for 10x)\n
+			Left/Right change selected note - Arrow Keys to change offset (Hold F for 10x)\n
 			Z + C/V - Copy & Paste", 16);
 		#else
 		var text:FlxText = new FlxText(0, 520, FlxG.width,
@@ -133,7 +133,7 @@ class NoteSplashDebugState extends MusicBeatState
 		FlxG.mouse.visible = true;
 
 		#if android
-		addVirtualPad(LEFT_FULL, A_B_C_X_Y_Z);
+		addVirtualPad(LEFT_FULL, A_B_C_D_V_X_Y_Z);
 		#end
 	}
 
