d2bee0198e97a8040bf2746af821c80e17afa3ad
nmslq <96362104+nmslq@users.noreply.github.com>
2023-08-10 16:42:59 +0800

    fix?

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -30,22 +30,22 @@ class HealthIcon extends FlxSprite
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-			var graphic = Paths.image(name, allowGPU);
-			
-			var width2 = width;
-			if (width2 == 450) {
-				loadGraphic(graphic, true, Math.floor(graphic.width / 3), Math.floor(graphic.height));
+			var file:Dynamic = Paths.image(name, allowGPU);
+			loadGraphic(file);
+
+			if (width == 450) {
+				loadGraphic(file, true, Math.floor(width / 3), Math.floor(height));
 				iconOffsets[0] = (width - 150) / 3;
 				iconOffsets[1] = (width - 150) / 3;
 				iconOffsets[2] = (width - 150) / 3;
 			} else {
-				loadGraphic(graphic, true, Math.floor(graphic.width / 2), Math.floor(graphic.height));
+				loadGraphic(file, true, Math.floor(width / 2), Math.floor(height));
 				iconOffsets[0] = (width - 150) / 2;
-				iconOffsets[1] = (height - 150) / 2;
+				iconOffsets[1] = (width - 150) / 2;
 			}
 			updateHitbox();
 
-			if (width2 == 450)
+			if (width == 450)
 				animation.add(char, [0, 1, 2], 0, false, isPlayer);
 			else
 				animation.add(char, [0, 1], 0, false, isPlayer);
