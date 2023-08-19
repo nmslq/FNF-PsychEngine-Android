e375750e47562addfdaded977b2e46a57a6e38fb
nmslq <96362104+nmslq@users.noreply.github.com>
2023-08-19 09:10:44 +0800

    Add files via upload

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -101,7 +101,7 @@ class AtlasFrameMaker extends FlxFramesCollection
 		var bitMapArray:Array<BitmapData> = [];
 		var daFramez:Array<FlxFrame> = [];
 		var firstPass = true;
-		var frameSize:FlxPoint = new FlxPoint(0, 0);
+		var frameSize:FlxPoint = FlxPoint.get(0, 0);
 
 		for (i in t.getFrame(animation)...t.numFrames)
 		{
@@ -141,6 +141,7 @@ class AtlasFrameMaker extends FlxFramesCollection
 			theFrame.frame = new FlxRect(0, 0, bitMapArray[i].width, bitMapArray[i].height);
 			daFramez.push(theFrame);
 			//trace(daFramez);
+			frameSize.put();
 		}
 		return daFramez;
 	}
