185ef3277b85227cc95dd6f97f1b0466b328e065
nmslq <96362104+nmslq@users.noreply.github.com>
2023-08-22 13:09:35 +0800

    Automated VersionShit

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -28,6 +28,12 @@ class MainMenuState extends MusicBeatState
 		'options'
 	];
 
+	private var versionShitInt:Int = 1;
+	private var versionShitArray:Array<Array<Dynamic>> = [// Name, Version, X, Y
+			["Psych Engine v", psychEngineVersion, null, null],
+			["Friday Night Funkin' v", Application.current.meta.get('version'), null, null]
+	];
+
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 
@@ -76,9 +82,6 @@ class MainMenuState extends MusicBeatState
 		add(menuItems);
 
 		var scale:Float = 1;
-		/*if(optionShit.length > 6) {
-			scale = 6 / optionShit.length;
-		}*/
 
 		for (i in 0...optionShit.length)
 		{
@@ -103,14 +106,20 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
-		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
-		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
+		versionShitArray.reverse();
+		for (i in 0...versionShitArray.length)
+		{
+			if (versionShitArray[i][1] == null) versionShitArray[i][1] = "";
+
+			var versionShit:FlxText = new FlxText(12, FlxG.height - 22 * versionShitInt, 0, versionShitArray[i][0] + versionShitArray[i][1]);
+			versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			versionShit.scrollFactor.set();
+			versionShit.x += versionShitArray[i][2];
+			versionShit.y -= versionShitArray[i][3];
+			add(versionShit);
+
+			versionShitInt++;
+		}
 
 		// NG.core.calls.event.logEvent('swag').send();
 
