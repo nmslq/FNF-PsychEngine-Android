bb12a32d3438704644805943eb0e6a11009264e7
nmslq <96362104+nmslq@users.noreply.github.com>
2023-09-08 22:07:37 +0800

    experimental updates
    1. Reworked Achievements menu
    2. Reset achievement button
    3. Add support for FeralInteractive Gamemode on Linux
    4. Icon workaround fix on Linux
    5. Pixel achievements/credits icons

--- source/android/flixel/FlxVirtualPad.hx
+++ source/android/flixel/FlxVirtualPad.hx
@@ -28,6 +28,7 @@ enum FlxActionMode
 	B;
 	A_B;
 	A_B_C;
+	B_C;
 	A_B_E;
 	A_B_X_Y;
 	A_B_C_X_Y;
@@ -123,6 +124,9 @@ class FlxVirtualPad extends FlxSpriteGroup
 				add(buttonC = createButton(FlxG.width - 384, FlxG.height - 135, 'c', 0x44FF00));
 				add(buttonB = createButton(FlxG.width - 258, FlxG.height - 135, 'b', 0xFFCB00));
 				add(buttonA = createButton(FlxG.width - 132, FlxG.height - 135, 'a', 0xFF0000));
+			case B_C:
+				add(buttonC = createButton(FlxG.width - 258, FlxG.height - 135, 'c', 0x44FF00));
+				add(buttonB = createButton(FlxG.width - 132, FlxG.height - 135, 'b', 0xFFCB00));
 			case A_B_E:
 				add(buttonE = createButton(FlxG.width - 384, FlxG.height - 135, 'e', 0xFF7D00));
 				add(buttonB = createButton(FlxG.width - 258, FlxG.height - 135, 'b', 0xFFCB00));
